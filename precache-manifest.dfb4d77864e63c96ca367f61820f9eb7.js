5ad5923e67c088a26c342a333929321114d7e20c
EasymoneyF <55006262+EasymoneyF@users.noreply.github.com>
2020-08-11 09:53:53 -0700

    Updates

---
+++ precache-manifest.dfb4d77864e63c96ca367f61820f9eb7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96833fc7af9dc5ef46357c6b4c833900",
+    "url": "/ninja-app/index.html"
+  },
+  {
+    "revision": "4063c5e4650958353adc",
+    "url": "/ninja-app/static/css/main.31c6ba90.chunk.css"
+  },
+  {
+    "revision": "21853adf3327179adc26",
+    "url": "/ninja-app/static/js/2.f11b6b33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ninja-app/static/js/2.f11b6b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4063c5e4650958353adc",
+    "url": "/ninja-app/static/js/main.31c6ce96.chunk.js"
+  },
+  {
+    "revision": "585fe5fc184e262f6bf6",
+    "url": "/ninja-app/static/js/runtime-main.9a589334.js"
+  },
+  {
+    "revision": "26d04666c283b77dca840f6c52ab81a3",
+    "url": "/ninja-app/static/media/NightinTokyo.26d04666.ttf"
+  },
+  {
+    "revision": "d83a7d12e7042b8bd2b8e95d81797bdb",
+    "url": "/ninja-app/static/media/RagingRedLotusBB.d83a7d12.ttf"
+  },
+  {
+    "revision": "ec560f2c4b2ce5aa3195b59bf4d255a4",
+    "url": "/ninja-app/static/media/lilninja.ec560f2c.png"
+  },
+  {
+    "revision": "337e1f36226bc4ac63863b32e96e8bed",
+    "url": "/ninja-app/static/media/star.337e1f36.svg"
+  }
+]);
